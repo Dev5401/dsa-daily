61ae8a7cbf7425013c4e4aeef34eabbc1b85f03c
Dev5401 <mba21081998@gmail.com>
2025-07-07 13:01:11 +0530

    added bubble sort algorithim

---
+++ arrays/11-bubble-sort.js
@@ -0,0 +1,12 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([38, 27, 43, 3, 9, 82, 10]));
